ad4bd50f6a62a6ecff7ed1a889eaadeb5747018f
Christian <christian.urich@gmail.com>
2011-11-10 11:55:50 +0100

    added licence

--- src/core/DMsystem.h
+++ src/core/DMsystem.h
@@ -1,9 +1,42 @@
+/**
+ * @file
+ * @author  Chrisitan Urich <christian.urich@gmail.com>
+ * @author  Michael Mair <abroxos@gmail.com>
+ * @version 1.0
+ * @section LICENSE
+ * This file is part of DynaMite
+ *
+ * Copyright (C) 2011  Christian Urich, Michael Mair
+
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
 #ifndef SYSTEM_H
 #define SYSTEM_H
+
+
+
 
 #include <map>
 #include <vector>
 
+
+/**
+  @todo if someone wants to include system he has to include the component header first. Should be changed.
+  **/
 namespace DM {
 
     class Component;
