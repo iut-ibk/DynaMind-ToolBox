f073769bb795806f9db991479281757c2214e6d9
Christian Urich <christian.urich@gmail.com>
2012-03-09 11:15:46 +0100

    added plotvectordata

---
+++ src/qgis-plugin/qgisdynamind.cpp
@@ -0,0 +1,73 @@
+/**
+ * @file
+ * @author  Chrisitan Urich <christian.urich@gmail.com>
+ * @version 1.0
+ * @section LICENSE
+ *
+ * This file is part of DynaMind
+ *
+ * Copyright (C) 2012  Christian Urich
+ 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+#include "qgisdynamind.h"
+
+#ifdef WIN32
+#define QGISEXTERN extern "C" __declspec( dllexport )
+#else
+#define QGISEXTERN extern "C"
+#endif
+QGisDynaMind::QGisDynaMind(QgisInterface* iface): mIface(iface)
+{
+}
+QGisDynaMind::~QGisDynaMind()
+{
+}
+void QGisDynaMind::initGui()
+{
+}
+void QGisDynaMind::unload()
+{
+}
+QGISEXTERN QgisPlugin* classFactory(QgisInterface* iface)
+{
+    return new QGisDynaMind(iface);
+}
+QGISEXTERN QString name()
+{
+    return "DynaMind Integration";
+}
+QGISEXTERN QString description()
+{
+    return "Integrates DynaMind in QGis";
+}
+QGISEXTERN QString version()
+{
+    return "0.00001";
+}
+// Return the type (either UI or MapLayer plugin)
+QGISEXTERN int type()
+{
+    return QgisPlugin::UI;
+}
+// Delete ourself
+QGISEXTERN void unload(QgisPlugin* theQgsPointConverterPluginPointer)
+{
+    delete theQgsPointConverterPluginPointer;
+}
+
+
