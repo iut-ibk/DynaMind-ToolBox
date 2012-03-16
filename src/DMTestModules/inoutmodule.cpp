e7465a393fa05c7dd99e21f28db0edb968640153
Christian <christian.urich@gmail.com>
2012-03-16 20:50:16 +0100

    print number of items

--- src/DMTestModules/inoutmodule.cpp
+++ src/DMTestModules/inoutmodule.cpp
@@ -51,31 +51,10 @@ void InOut::run() {
     DM::Node * n1 = sys_in->addNode(0,0,2, inlets);
     DM::Node * n2 = sys_in->addNode(0,0,3, inlets);
 
-    double b = 0;
-
-    std::map<std::string, DM::System*> subs = sys_in->getAllSubSystems();
-
-    for (std::map<std::string, DM::System*>::const_iterator it = subs.begin(); it != subs.end(); ++it)
-    {
-        DM::System * s = it->second;
-        Logger(Debug) << s->getName() << " " << s->getAllNodes().size() << " " << s->getAllEdges().size();
-    }
-
-    /*for (int i = 0; i < test; i++) {
-        for (int j = 0; j < test; j++) {
-            for (int k = 0; k < test; k++)            {
-
-
-                        b++;
-
-
-            }
-        }
-    }
-    Logger(Debug) << b;
-    */
 
     sys_in->addEdge(n1, n2, conduits);
+
+    Logger(Debug) <<  "Number of stuff "<< sys_in->getAllEdges().size();
 
 
 }
