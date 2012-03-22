fa0579158b4ef631f1b5ee5955b3cd99a3d013ae
Christian <christian.urich@gmail.com>
2012-03-22 22:51:10 +0100

    export nodes and faces

--- scripts/Modules/ExportToShapeFile.py
+++ scripts/Modules/ExportToShapeFile.py
@@ -25,13 +25,11 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import osgeo.ogr, osgeo.osr
 import os
 from pydynamind import *
+from pydmtoolbox import *
 
 class ExportToShapeFile(Module):
             def __init__(self):
                 Module.__init__(self)
-                self.Points = True
-                self.Lines = True
-                self.Faces = True
  
                 #self.addParameter("Name", STRING, self.FileName)
                 self.createParameter("FileName", STRING,  "test")
@@ -44,14 +42,78 @@ class ExportToShapeFile(Module):
                 self.Lines = True
                 self.createParameter("Faces", BOOL,  "test")
                 self.Faces = False
-                self.vec = View("CONDUIT", EDGE, READ)
+                self.vec = View("dummy", SUBSYSTEM, READ)
                 views = []
                 views.append(self.vec)
                 self.addData("City", views)
                 
             def run(self):
-                self.exportPolyline()   
+                if self.Faces:                       
+                    self.exportFaces()
+                if self.Lines:
+                    self.exportPolyline()       
+                if self.Points:
+                    self.exportPoints()         
+
+            def exportFaces(self):
+		city = self.getData("City")
+                spatialReference = osgeo.osr.SpatialReference()
+                spatialReference.ImportFromProj4('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+                
+                #Init Shape Files
+                driver = osgeo.ogr.GetDriverByName('ESRI Shapefile')
+                if os.path.exists(str(self.FileName+'_faces.shp')): os.remove(self.FileName+'_faces.shp')
+                shapeData = driver.CreateDataSource(self.FileName+'_faces.shp')
+                
+                layer = shapeData.CreateLayer('layer1', spatialReference, osgeo.ogr.wkbPolygon)
+                layerDefinition = layer.GetLayerDefn()               
+                AttributeList = []
+                attr = []
+                hasAttribute = False
+                #Get Data 
+
+                 
+                names = city.getNamesOfComponentsInView(View(self.Name, READ, FACE))
+                for i in range(len(names)): 
+                    attributemap = city.getComponent(names[i]).getAllAttributes()
+                    for key in attributemap.keys():
+			if (key in attr) == False:
+                        	attr.append(key)
+                for j in range(len(attr)):
+		     hasAttribute = True
+                     if (attr[j] in AttributeList) == False:
+                         attribute = city.getComponent(names[i]).getAttribute(attr[j])
+                         fielddef = osgeo.ogr.FieldDefn(attr[j], osgeo.ogr.OFTReal)
+                         layer.CreateField(fielddef)
+                         layerDefinition = layer.GetLayerDefn()  
+                         AttributeList.append(attr[j])                         
+		for i in range(len(names)): 
+                    #Append Attributes
+                    alist = city.getComponent(names[i]).getAllAttributes().keys() 
+
                     
+                    face = city.getFace(names[i])
+                            
+                    line = osgeo.ogr.Geometry(osgeo.ogr.wkbPolygon)
+                    ring = osgeo.ogr.Geometry(osgeo.ogr.wkbLinearRing)
+                    nl =  TBVectorData.getNodeListFromFace(city, face)
+                    for p in nl:
+                        ring.AddPoint(p.getX(),p.getY())
+                    line.AddGeometry(ring)
+
+                    featureIndex = 0
+                    feature = osgeo.ogr.Feature(layerDefinition)
+                    feature.SetGeometry(line)
+                    feature.SetFID(featureIndex)  
+                    #Append Attributes
+                    if hasAttribute:        
+                        for k in range(len(alist)):
+                              value = city.getComponent(names[i]).getAttribute(alist[k]).getDouble()
+                              feature.SetField(alist[k],value)
+                    layer.CreateFeature(feature)    
+
+                shapeData.Destroy()    
+                         
             def exportPolyline(self):
                 city = self.getData("City")
                 spatialReference = osgeo.osr.SpatialReference()
@@ -66,14 +128,12 @@ class ExportToShapeFile(Module):
                 attr = []
                 hasAttribute = False
                 #Get Data 
-                names = city.getNamesOfComponentsInView(self.vec)
+                names = city.getNamesOfComponentsInView(View(self.Name, READ, EDGE))
                 if len(names) > 0:
                     attributemap = city.getComponent(names[0]).getAllAttributes()
-                    print attributemap
                     for key in attributemap.keys():
                         attr.append(key)
                 for i in range(len(names)):
-                    print names[i]
                     #Append Attributes
                     alist = city.getComponent(names[i]).getAllAttributes().keys() 
                     for j in range(len(alist)):
@@ -103,5 +163,63 @@ class ExportToShapeFile(Module):
                                  value = edge.getAttribute(alist[k]).getDouble()
                                  feature.SetField(alist[k],value)
                     layer.CreateFeature(feature)    
-                shapeData.Destroy()               
+                shapeData.Destroy()  
+ 
+            def exportPoints(self):
+		city = self.getData("City")
+                spatialReference = osgeo.osr.SpatialReference()
+                spatialReference.ImportFromProj4('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+                
+                #Init Shape Files
+                driver = osgeo.ogr.GetDriverByName('ESRI Shapefile')
+                if os.path.exists(str(self.FileName+'_points.shp')): os.remove(self.FileName+'_points.shp')
+                shapeData = driver.CreateDataSource(self.FileName+'_points.shp')
+                
+                layer = shapeData.CreateLayer('layer1', spatialReference, osgeo.ogr.wkbPoint)
+                #osgeo.ogr.wkbPolygon()
+                layerDefinition = layer.GetLayerDefn()               
+                AttributeList = []
+                attr = []
+                hasAttribute = False
+                
+                fielddef = osgeo.ogr.FieldDefn("Z", osgeo.ogr.OFTReal)
+                layer.CreateField(fielddef)
+                layerDefinition = layer.GetLayerDefn()  
+                AttributeList.append("Z") 
+                
+                names = city.getNamesOfComponentsInView(View(self.Name, READ, NODE))
+                for i in range(len(names)): 
+                    attributemap = city.getComponent(names[i]).getAllAttributes()
+                    for key in attributemap.keys():
+			if (key in attr) == False:
+                        	attr.append(key)
+                for j in range(len(attr)):
+		     hasAttribute = True
+                     if (attr[j] in AttributeList) == False:
+                         attribute = city.getComponent(names[i]).getAttribute(attr[j])
+                         fielddef = osgeo.ogr.FieldDefn(attr[j], osgeo.ogr.OFTReal)
+                         layer.CreateField(fielddef)
+                         layerDefinition = layer.GetLayerDefn()  
+                         AttributeList.append(attr[j])                         
+		for i in range(len(names)): 
+                    #Append Attributes
+                    alist = city.getComponent(names[i]).getAllAttributes().keys()                          
+                        
+                            
+                    #Addend Points
+                    node = city.getNode(names[i])
+                    point = osgeo.ogr.Geometry(osgeo.ogr.wkbPoint)
+                    point.SetPoint(0, node.getX(),  node.getY())
+                    featureIndex = 0
+                    feature = osgeo.ogr.Feature(layerDefinition)
+                    feature.SetGeometry(point)
+                    feature.SetFID(featureIndex) 
+                    feature.SetField("Z", node.getZ())
+                    #Append Attributes
+                    if hasAttribute:        
+                        for k in range(len(alist)):
+                              value = city.getComponent(names[i]).getAttribute(alist[k]).getDouble()
+                              feature.SetField(alist[k],value)
+                    layer.CreateFeature(feature)  
+                shapeData.Destroy()            
                
