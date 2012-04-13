f1e2315d47d87f36926ad3406ac66b3c2c38dcfa
Michael Mair <michael.mair@uibk.ac.at>
2012-04-13 15:47:47 +0200

    Update debian control files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(vibe2core):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="vibe2core" command="/usr/bin/vibe2core"
--- debian/vibe2core.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the vibe2core package
-#
-0 4	* * *	root	[ -x /usr/bin/vibe2core_maintenance ] && /usr/bin/vibe2core_maintenance
