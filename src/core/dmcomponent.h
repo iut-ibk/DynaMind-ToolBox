3e1c4a3009d7bbcbf3ce0fde46041ba836cb9da9
Christian Urich <christian.urich@gmail.com>
2012-05-13 19:05:47 +0200

    added doc

--- src/core/dmcomponent.h
+++ src/core/dmcomponent.h
@@ -45,7 +45,7 @@ namespace DM {
   *
   * All Objects like Nodes, Edges and Faces are derived from the component class.
   * Ever component can be identified by a unique ID created by the QT framework.
-  * Components also manages the attributes.
+  * Components also manages the attributes. Components can be part of none or more views.
   */
 class Attribute;
 
@@ -64,32 +64,73 @@ protected:
 
 
 public:
+    /** @brief create a new component
+      *
+      * The default constructor creates a UUID for the component.
+     */
     Component();
+    /** @brief Copies a component, also the UUID is copied! */
     Component(const Component& s);
     virtual ~Component();
+
+    /** @brief setUUID */
     void setUUID(std::string uuid);
+
+    /** @brief return UUID */
     std::string getUUID();
 
+    /** @brief adds a new Attribute to the Component.
+      *
+      * Returns true if the attribute has been added to the Component.
+      * Returns false if the attribute with the same name already exists.
+      */
     bool addAttribute(Attribute newattribute);
-    /** @brief add attribute to the component. If the Attribute already exists changeAttribute is called */
+
+    /** @brief Add new double attribute to the component. If the Attribute already exists changeAttribute is called */
     bool addAttribute(std::string, double val);
+
+    /** @brief Change existing Attribute. If attribute doesn't exist a new Attribute is added to the Component*/
     bool changeAttribute(Attribute newattribute);
+
+    /** @brief Change existing double Attribute. It the Attribute doesn't exist a new double Attribute is added*/
     bool changeAttribute(std::string, double val);
+
+    /** @brief Remove Attribute, returns false if no Attribute with this name exists */
     bool removeAttribute(std::string name);
+
+    /** @brief Returns a pointer to an Attribute */
     Attribute* getAttribute(std::string name);
 
     /** @brief Returns a map of all Attributes */
     const std::map<std::string, Attribute*> & getAllAttributes() const;
+
+
+    /** @brief adds Component to a View by using the name of the view */
+    void setView(std::string view);
+
+    /** @brief adds Component to a View.
+      *
+      * Therefore the name of the View is used as identifier.
+      */
+    void setView(const DM::View & view);
+
+    /** @brief Create new UUID */
+    void createNewUUID();
+
+    /** @brief Retruns a set of Views in which the Compont is used */
+    std::set<std::string> const &  getInViews() const;
+
+    /** @brief Returns true if Component is in the View */
+    bool isInView(DM::View view) const;
+
+
+
+
     bool addChild(Component *newcomponent);
     bool changeChild(Component *newcomponent);
     bool removeChild(std::string name);
     Component* getChild(std::string name);
     std::map<std::string, Component*> getAllChilds();
-    void setView(std::string view);
-    void setView(const DM::View & view);
-    void createNewUUID();
-    std::set<std::string> const &  getInViews() const;
-    bool isInView(DM::View view) const;
     virtual Component* clone()=0;
 
 };
