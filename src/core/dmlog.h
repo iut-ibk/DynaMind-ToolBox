99cccbce8ebbc8b551ea9443db475a2ee22ded9b
Christian Urich <christian.urich@gmail.com>
2012-05-21 11:41:07 +0200

    added Doc to Log

--- src/core/dmlog.h
+++ src/core/dmlog.h
@@ -33,37 +33,67 @@
 using namespace std;
 
 namespace DM {
-    //class Node;
-    class Simulation;
-    class Logger;
-    class LogSink;
+//class Node;
+class Simulation;
+class Logger;
+class LogSink;
 
-    enum LogLevel {
-        Debug = 0,
-        Standard = 1,
-        Warning = 2,
-        Error = 3
-            };
+enum LogLevel {
+    Debug = 0,
+    Standard = 1,
+    Warning = 2,
+    Error = 3
+};
 
 #define LOG_HEAD \
     if (level <= max) return *this;\
-            out << logLevel() << ": " << "\n"
+    out << logLevel() << ": " << "\n"
+/**
+ * @ingroup DynaMind-Core
+ * @brief DynaMind Log
+ *
+ * The Logger is implemented as singleton. The Logger need to initialsed by calling the init function.
+ * With the init function a LogSink is registered and the max LogLevel is defined.
+ * Following LogLevels can be used:
+ * - Debug
+ * - Standard
+ * - Warning
+ * - Error
+ * Error is the highest and Debug the lowest LogLevel. Only messages with the current LogLevel or higher are send to the sink.
+ * After Log is initalised messages the logger can be used like DM::Logger(DM::Standard) << "Test"
+ *
+ * Sample code to redirect logging to cout
+ * @code
+ * //redirect out to cout
+ * ostream *out = &cout;
+ * //Create a new LogSink (cout) and use the Standard LogLevel
+ * DM::Log::init(new DM::OStreamLogSink(*out), DM::Standard);
+ * DM::Logger(DM::Standard) << "Hello Logger";
+ * @endcode
+ *
+ */
+class DM_HELPER_DLL_EXPORT Log
+{
+public:
+    /** @brief Initialise Logger
+     *
+     * If no instance of the logger exists. A new instance is created.
+     * Log takes ownership of the instance
+     */
+    static void init(LogSink *sink, LogLevel max = Debug);
+    /** @brief Delets sink and instance*/
+    static void shutDown();
+    /** @brief Returns the current Instance */
+    static Log *getInstance();
+    friend class Logger;
+private:
+    Log();
+    virtual ~Log();
 
-            class DM_HELPER_DLL_EXPORT Log
-    {
-    public:
-        static void init(LogSink *sink, LogLevel max = Debug);
-        static void shutDown();
-        static Log *getInstance();
-        friend class Logger;
-    private:
-        Log();
-        virtual ~Log();
+    static Log *instance;
 
-        static Log *instance;
-
-        LogSink *sink;
-        LogLevel max;
-    };
+    LogSink *sink;
+    LogLevel max;
+};
 }
 #endif // LOG_H
