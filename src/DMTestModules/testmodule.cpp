ae3d69a0fa71757292ad77950fc1a0f38d965402
Markus Sengthaler <markus.sengthaler@student.uibk.ac.at>
2012-10-29 12:02:57 +0100

    reduced rastersize (poor sql perf)

--- src/DMTestModules/testmodule.cpp
+++ src/DMTestModules/testmodule.cpp
@@ -82,7 +82,7 @@ void TestModule::run() {
 
     outputRasterData = this->getRasterData("RasterData", rdata);
 
-    outputRasterData->setSize(200, 200, 20);
+    outputRasterData->setSize(5, 5, 20);
     for (unsigned int i = 0; i < outputRasterData->getWidth(); i++) {
         for (unsigned int j = 0; j < outputRasterData->getHeight(); j++) {
             outputRasterData->setValue(i,j,15);
