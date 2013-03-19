5f6528877591433788062cb5db7b4bc0dc00377d
Markus Sengthaler <m.sengthaler@gmail.com>
2013-03-19 17:00:33 +0100

    added several parameters to executable

--- main.cpp
+++ main.cpp
@@ -45,6 +45,7 @@
 
 #include <ostream>
 #include <dmlog.h>
+#include <dmdbconnector.h>
 
 using namespace std;
 //using namespace boost::python;
@@ -56,9 +57,6 @@ typedef boost::error_info<struct errinfo_type_info_name_,std::string> errinfo_ty
 int main(int argc, char *argv[], char *envp[]) {
     QThreadPool::globalInstance()->setMaxThreadCount(1);
 
-    ostream *out = &cout;
-    DM::Log::init(new DM::OStreamLogSink(*out), DM::Standard);
-    DM::Logger(DM::Standard) << "start";
 
     po::positional_options_description p;
     p.add("input-file", -1);
@@ -70,53 +68,110 @@ int main(int argc, char *argv[], char *envp[]) {
             ("help", "produce help message")
             ("input-file", po::value<string>(), "set simulation file")
             ("repeat", po::value<int>(), "repeat simulation")
-            ("python-modules", po::value<vector <string> >(), "set path to python modules")
+            ("verbose", "verbose output")
+            ("nodecache", po::value<unsigned long>(), "node cache size")
+            ("attributecache", po::value<unsigned long>(), "attribute cache size")
+            ("sqlquerystack", po::value<unsigned long>(), "sql query cache size")
+            ("blockwriting", po::value<unsigned long>(), "sql write block size")
+            ("loglevel", po::value<int>(), "logger level 0-3 (Debug-Standard-Warning-Error)")
+            //("python-modules", po::value<vector <string> >(), "set path to python modules")
             ;
     std::string simulationfile;
     std::vector<std::string> pythonModules;
-    int repeat;
-
+    int repeat = 1;
+	bool verbose = false;
+	DM::LogLevel ll = DM::Standard;
     try {
 
         po::store(po::command_line_parser(argc, argv).
                   options(desc).positional(p).run(), vm);
         po::notify(vm);
 
-        if (vm.count("help")) {
+		verbose = vm.count("verbose");
+
+        if (vm.count("help")) 
+		{
             cout << desc << "\n";
             return 1;
         }
 
-        if (vm.count("input-file")) {
+        if (vm.count("input-file"))
             simulationfile =  vm["input-file"].as<string>();
-        }  else {
+        else 
+		{
             std::cout << "Simulation file not set" << std::endl;
             return -1;
         }
-        if (vm.count("python-modules")) {
-            pythonModules =  vm["python-modules"].as<vector <string> >();
-        }
-        if (vm.count("repeat")) {
-            repeat =  vm["repeat"].as<int> ();
-        }
+        //if (vm.count("python-modules")) {
+        //    pythonModules =  vm["python-modules"].as<vector <string> >();
+        //}
+        if (vm.count("repeat"))		repeat = vm["repeat"].as<int>();
+        if (vm.count("loglevel"))		ll = (DM::LogLevel)vm["loglevel"].as<int>();
 
+		
+		ostream *out = &cout;
+		DM::Log::init(new DM::OStreamLogSink(*out), ll);
+
+		DM::DBConnectorConfig cfg;
+		if(vm.count("nodecache"))		cfg.nodeCacheSize		 = vm["nodecache"].as<unsigned long>();
+		if(vm.count("attributecache"))	cfg.attributeCacheSize	 = vm["attributecache"].as<unsigned long>();
+		if(vm.count("sqlquerystack"))	cfg.queryStackSize		 = vm["sqlquerystack"].as<unsigned long>();
+		if(vm.count("blockwriting"))	cfg.cacheBlockwritingSize = vm["blockwriting"].as<unsigned long>();
+		DM::DBConnector::getInstance()->setConfig(cfg);
+
+		if(verbose)
+		{
+			std::cout << ">> config:" << endl;
+			std::cout << "node cache size: " << cfg.nodeCacheSize << endl;
+			std::cout << "attribute cache size: " << cfg.attributeCacheSize << endl;
+			std::cout << "sql query stack size: " << cfg.queryStackSize << endl;
+			std::cout << "sql write block size: " << cfg.cacheBlockwritingSize << endl;
+		}
     }
-    catch (po::unknown_option & e){
+    catch (po::unknown_option & e)
+	{
         std::cout << "unknown option" << std::endl;
         cout << desc << "\n";
-
-
     }
-    //modified
+	
+	if(!QFile::exists(QString::fromStdString(simulationfile)))
+	{
+		std::cout << "simulation file not found" << std::endl;
+		return -1;
+	}
 	
     DM::Simulation s;
-	std::cout << "loading modules" << std::endl;
+	std::cout << ">> loading modules" << std::endl;
 	s.loadModulesFromDefaultLocation();
-	std::cout << "loading simulation file" << std::endl;
+	std::cout << ">> loading simulation file" << std::endl;
     s.loadSimulation(simulationfile);
-	std::cout << "starting simulation" << std::endl;
-    s.run();
-	std::cout << "finished simulation" << std::endl;
+	DM::Logger(DM::Standard) << ">> starting simulation";
+
+	std::list<qint64> times;
+
+	QElapsedTimer timer;
+	for(int i=0;i<repeat;i++)
+	{
+		s.resetSimulation();
+		if(verbose && repeat>1)
+			std::cout << "iteration " << i << endl;
+		timer.start();
+		s.run();
+		times.push_back(timer.elapsed());
+		if(verbose && repeat>1)
+			std::cout << "iteration " << timer.elapsed() << endl;
+	}
+	qint64 avg = 0;
+	foreach(qint64 i, times)
+		avg += i;
+	avg /= times.size();
+
+	qint64 sigma = 0;
+	foreach(qint64 i, times)
+		sigma += (avg-i)*(avg-i)/max(1,repeat-1);
+	sigma = sqrt((float)sigma);
+
+	std::cout << ">> finished simulation at an average of " << avg << "+-"<< sigma << "ms" << std::endl;
     QThreadPool::globalInstance()->waitForDone();
 
 }
