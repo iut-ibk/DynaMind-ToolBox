06a0360a95fc91ece7599a2f58f598f28aa22f58
Markus Sengthaler <m.sengthaler@gmail.com>
2013-08-08 12:10:12 +0200

    added license, fixed formating, cleanup

--- src/dmcolorbarwidget.cpp
+++ src/dmcolorbarwidget.cpp
@@ -1,65 +1,88 @@
+/**
+* @file
+* @author  Chrisitan Urich <christian.urich@gmail.com>
+* @version 1.0
+* @section LICENSE
+*
+* This file is part of DynaMind
+*
+* Copyright (C) 2011-2012  Christian Urich, Markus Sengthaler
+
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*
+*/
+
 #include "dmcolorbarwidget.h"
 #ifdef __APPLE__
 #include <OpenGL/glu.h>
 #else
 #ifdef _WIN32
-  #include <windows.h>
+#include <windows.h>
 #endif
 #include <GL/glu.h>
 #endif
 
-namespace DM {
+using namespace DM;
 
 ColorBarWidget::ColorBarWidget(QWidget *parent) :
-    QGLWidget(parent) 
+	QGLWidget(parent) 
 {
 	texture = 0;
 }
 
 ColorBarWidget::ColorBarWidget(QWidget *parent, QGLWidget *shared) :
-    QGLWidget(parent, shared)  
+	QGLWidget(parent, shared)  
 {
 	texture = 0;
 }
 
 void ColorBarWidget::initializeGL() 
 {
-    glClearColor(0, 0.0, 0, 1.0);
+	glClearColor(0, 0.0, 0, 1.0);
 }
 
 void ColorBarWidget::paintGL() 
 {
-    //glClear(GL_COLOR_BUFFER_BIT);
-    if (!glIsTexture(texture))
-        return;
+	if (!glIsTexture(texture))
+		return;
 
-    glEnable(GL_TEXTURE_1D);
-    glBindTexture(GL_TEXTURE_1D, texture);
-    
-    glBegin(GL_QUADS);
-        glColor3f(1.0, 1.0, 1.0);
-        
-        glTexCoord1f(0.0);
-        glVertex2f(0, height());
-        
-        glTexCoord1f(1.0);
-        glVertex2f(width(), height());
-    
-        glTexCoord1f(1.0);
-        glVertex2f(width(), 0);
-        
-        glTexCoord1f(0.0);
-        glVertex2f(0, 0);
-    glEnd();
-    
-    glDisable(GL_TEXTURE_1D);
+	glEnable(GL_TEXTURE_1D);
+	glBindTexture(GL_TEXTURE_1D, texture);
+
+	glBegin(GL_QUADS);
+	glColor3f(1.0, 1.0, 1.0);
+
+	glTexCoord1f(0.0);
+	glVertex2f(0, height());
+
+	glTexCoord1f(1.0);
+	glVertex2f(width(), height());
+
+	glTexCoord1f(1.0);
+	glVertex2f(width(), 0);
+
+	glTexCoord1f(0.0);
+	glVertex2f(0, 0);
+	glEnd();
+
+	glDisable(GL_TEXTURE_1D);
 }
 
 void ColorBarWidget::resizeGL(int w, int h) {
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0, w, 0, h);
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, w, 0, h);
 }
-
-} // namespace DM
