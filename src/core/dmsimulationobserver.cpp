ee3b669066a8177b9fd4452dde0c8d797bd13fff
Markus Sengthaler <m.sengthaler@gmail.com>
2013-08-14 14:21:08 +0200

    cleanup of simulationobserver

--- src/core/dmsimulationobserver.cpp
+++ src/core/dmsimulationobserver.cpp
@@ -23,10 +23,5 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  */
+
 #include "dmsimulationobserver.h"
-/*
-using namespace DM;
-SimulationObserver::SimulationObserver()
-{
-}
-*/
