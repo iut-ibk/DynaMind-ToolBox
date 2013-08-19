144e406b09790c0bea3e9e6b4173e2f65c836f43
Christian Urich <christian.urich@gmail.com>
2013-08-19 11:13:13 +1000

    added unit tests from complex_unit tests

---
+++ unit-tests/unittestsdmextensions.h
@@ -0,0 +1,12 @@
+#ifndef UNITTESTSDMEXTENSIONS_H
+#define UNITTESTSDMEXTENSIONS_H
+
+#include <gtest/gtest.h>
+
+class UnitTestsDMExtensions : public ::testing::Test
+{
+public:
+
+};
+
+#endif // UNITTESTSDMEXTENSIONS_H
