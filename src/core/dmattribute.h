1b8bf73e6c30e43fa50b46db14b01d5b0bd386f2
Markus Sengthaler <m.sengthaler@gmail.com>
2013-10-03 15:11:18 +0200

    reformated attribute values

--- src/core/dmattribute.h
+++ src/core/dmattribute.h
@@ -198,8 +198,8 @@ public:
 	static void ClearCache();
 
 private:
-	QUuid		_uuid;
-	std::string name;
+	QUuid			_uuid;
+	std::string		name;
 	Component*		owner;
 	AttributeValue	*value;
 	bool			isInserted;
