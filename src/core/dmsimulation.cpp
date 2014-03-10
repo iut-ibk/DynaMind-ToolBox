08f0b13c1b4ed6cb49cc87e7bad3dcba79c9c671
Markus Sengthaler <m.sengthaler@gmail.com>
2014-03-10 17:26:36 +0100

    changed failed to load mod. msg to warning (isntead of debug)

--- src/core/dmsimulation.cpp
+++ src/core/dmsimulation.cpp
@@ -161,7 +161,7 @@ bool Simulation::registerModule(const std::string& filepath)
 		}
 		else
 		{
-            Logger(Debug) <<  "failed loading native module " << filepath;
+            Logger(Warning) <<  "failed loading native module " << filepath;
 			return false;
 		}
 	}
