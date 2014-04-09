c5eed149ac6beb5d574f3e3cabaf10b43ff514d2
Markus Sengthaler <m.sengthaler@gmail.com>
2014-04-09 14:19:58 +0200

    cleanup

--- src/DynaMind-BasicModules/export.h
+++ src/DynaMind-BasicModules/export.h
@@ -28,7 +28,6 @@
 #define EXPORT_H
 
 #include <dmmodule.h>
-//#include <dm.h>
 
 class OGRLayer;
 class OGRDataSource;
@@ -48,7 +47,6 @@ public:
 	void init();
 	void reset();
 	//virtual bool  createInputDialog();
-	//string getHelpUrl();
 private:
 	std::string path;
 	int epsgCode;
@@ -57,121 +55,5 @@ private:
 	OGRLayer* prepareNewLayer(const DM::View& view, OGRDataSource* data);
 	void exportLayer(const DM::View& view, OGRLayer* layer, System* system);
 };
-
-/*
-class DM_HELPER_DLL_EXPORT Import : public Module
-{
-	DM_DECLARE_NODE(Import)
-
-public:
-	// parameters
-	std::string FileName;
-
-	std::string WFSServer;
-	std::string WFSUsername;
-	std::string WFSPassword;
-
-	int		epsgcode;
-
-	bool	append;
-	bool	linkWithExistingView;
-	bool	flip_wfs;
-
-	double	offsetX;
-	double	offsetY;
-
-private:
-	// backup states
-	bool		append_old;
-	std::string FileName_old;
-	std::string ViewName_old;
-	std::string WFSServer_old;
-	std::string WFSUsername_old;
-	std::string WFSPassword_old;
-
-	// internal settings
-	double	devider;
-	double	tol;
-
-	QHash<QString, std::vector<DM::Node* > > nodeList;
-
-	enum DRIVERTYPE {
-		DataError,
-		ShapeFile,
-		WFS,
-		RasterData,
-	}driverType;
-
-public:
-	// public for ui
-	void initViews();
-
-	void reloadFile();
-
-	SimpleCrypt crypto;
-private:
-	// main methods
-	static bool ExtractLayers(OGRDataSource* dataSource, StringMap& viewConfig, 
-		std::map<std::string, int>& viewConfigTypes, StringMap& viewEPSGConfig,
-		int targetEPSG);
-
-	static bool ExtractLayers(GDALDataset* dataSource, StringMap& newViewConfig, 
-		std::map<std::string, int>& newViewConfigTypes);
-
-	void adoptViewConfig(StringMap& newViewConfig, std::map<std::string, int>& newViewConfigTypes);
-
-	void loadVectorData(const std::string& path);
-
-	bool loadRasterData();
-
-	// OGR-geom. loading methods
-	void loadLayer(OGRLayer* layer, System* sys);
-
-	void loadPoint(System *sys, OGRPoint *point, OGRCoordinateTransformation *poCT,
-		OGRFeatureDefn* featureDef, OGRFeature* curFeature, const DM::View& view);
-
-	void loadLineString(System *sys, OGRLineString *lineString, OGRCoordinateTransformation *poCT,
-		OGRFeatureDefn* featureDef, OGRFeature* curFeature, const DM::View& view);
-
-	void loadPolygon(System *sys, OGRPolygon *polygon, OGRCoordinateTransformation *poCT,
-		OGRFeatureDefn* featureDef, OGRFeature* curFeature, const DM::View& view);
-
-	// helper methods
-	DM::Node* addNode(DM::System* sys, double x, double y, OGRCoordinateTransformation *poCT,
-		const View* view = NULL);
-
-	std::vector<Node*> addFaceNodes(System* sys, const OGRLineString *ring,
-		OGRCoordinateTransformation *poCT);
-
-	void appendAttributes(DM::Component * cmp, OGRFeatureDefn *poFDefn, OGRFeature *poFeature, const View& view);
-
-	bool moduleParametersChanged();
-
-	// geo helpers
-	void initPointList(DM::System * sys);
-
-	QString createHash(double x, double y);
-
-	bool transform(double *x, double *y, OGRCoordinateTransformation *poCT);
-
-	std::string getServerPath();
-public:
-	// parameter map: layername[.attribute], new-view-name
-	StringMap viewConfig;
-
-	// layer[.attribute], type; integer to combine attributes and views
-	std::map<std::string, int> viewConfigTypes;
-
-	// layername, epsg (as string, for param.)
-	StringMap viewEPSGConfig;
-public:
-	void run();
-	void init();
-	void reset();
-	virtual bool  createInputDialog();
-	string getHelpUrl();
-	Import();
-	~Import();
-};*/
 
 #endif // EXPORT_H
