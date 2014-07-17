ab7092cab61e6110b22277db13e71b1cf5230a79
Christian Mikovits <christian.mikovits@uibk.ac.at>
2014-07-17 09:30:37 +0200

    waterattributes module 1st rev

--- src/urbandevelWaterAttributes.h
+++ src/urbandevelWaterAttributes.h
@@ -4,33 +4,18 @@
 #include <dm.h>
 #include <dmcompilersettings.h>
 
-using namespace DM;
-class DM_HELPER_DLL_EXPORT urbandevelWaterAttributes : public Module
+class urbandevelWaterAttributes : public DM::Module
 {
     DM_DECLARE_NODE(urbandevelWaterAttributes);
 
 private:
     DM::View city;
-    DM::View houses;
-    DM::View parcels;
-    DM::View building_model;
-    DM::View footprint;
+    DM::View buildingview;
 
-
-    int buildingyear;
-    int stories;
-    double width;
-    double ratio;
-    bool onSignal;
-    bool rotate90;
     bool paramfromCity;
-    bool createPopulation;
-    bool create3DGeometry;
-    double dwf_per_person;
-    double wsd_per_person;
-    double space_required;
+    double dwfpp;
+    double wsdpp;
 
-    void createWindows(DM::Face * f, double distance, double width, double height);
 public:
     urbandevelWaterAttributes();
     void run();
