bc75015984e1f487ba2b7782f1f9d6cc00c9be01
Christian Mikovits <christian.mikovits@uibk.ac.at>
2014-11-25 16:16:06 +0100

    converted file included + comments in script

--- src/scripts/uvf2dat.r
+++ src/scripts/uvf2dat.r
@@ -1,9 +1,9 @@
 ###
-### Convert UVF File format to DAT (SWMM5) Files
+### Convert UVF file format to DAT (swmm5) files
 ###
-### Input: Folder with UVF Files
+### Input: directory with UVF files
 ###
-### Output: DAT Files in the same Folder
+### Output: DAT files in the same directory
 ###
 
 # cleanup
@@ -11,16 +11,19 @@
 rm(list=ls())
 
 # read
+
 filepath<-("/home/gaelic/Code/DynaMind/DynaMind-DynAlp/data") # the path must consist another folder named UVF and DAT
 setwd(filepath)
 
 list<-dir(pattern="*.uvf")
 
+# convert and write
+
 for (n in 1:length(list))
   {
     file<-list[n]
     
-    # erst nur die Zeilen einlesen, in der die Jahrhunderte, die Stationsnr & die Koordinaten und die Anfangs- und Endzeit stehen
+    # read information lines
     infos<-read.table(file,header=F,sep=" ",dec=".",comment.char="$",nrows=3,skip=4,flush=F,strip.white=T,fill=T)
     
     cent_start<-infos[1,17] # Anfangsjahrhundert
@@ -28,8 +31,10 @@ for (n in 1:length(list))
     time_start<-substr(infos[3,1],1,10)   # Anfangszeitpunkt
     time_end<-substr(infos[3,1],11,20)  # Endzeitpunkt
     
-    # ganze Tabelle einlesen, ab den valueserten
-    values<-read.table(file,header=F,sep=" ",dec=".", col.names=c("T","mm"),colClasses=c("character","character"),comment.char="$",skip=7) #,nrows=500
+    # read values
+    values<-read.table(file,header=F,sep=" ",dec=".", col.names=c("T","mm"),colClasses=c("character","character"),comment.char="$",skip=7)
+    
+    # check sanity
     
     if(values[dim(values)[1],1]!=time_end || values[1,1]!=time_start)
     {
@@ -37,16 +42,16 @@ for (n in 1:length(list))
       next
     }
     
-    Y<-substr(values$T,1,2) # alle Jahreszahlen herausholen
+    Y<-substr(values$T,1,2) # get year values
     
-    if (cent_start==cent_end)   # wenn AnfangsJHD und EndJHD gleich sind, dann bei jedem Zeitpunkt der Messdaten die ersten beiden Zahlen davor anf?gen, um auf normale Jahreszahlen zu kommen
+    if (cent_start==cent_end)   # if values are equal -> same century
       {
         Y<-paste(substr(cent_start,1,2),Y,sep="")
       }
     
-    if (cent_start<cent_end && values$T[1]>50)  # wenn AnfangsJHD kleiner als EndJHD, dann ab dem Wert, der mit 00 (f?r 2000) anf?ngt, 19 davorsetzen, danach 20
+    if (cent_start<cent_end && values$T[1]>50)  # if century switch
       {
-        for(i in 2:length(Y))     # letzter Index des alten Jahrhunderts herausfinden
+        for(i in 2:length(Y))     # filter last index of old century
           {
             if(Y[i]<Y[i-1])
             {
@@ -54,12 +59,13 @@ for (n in 1:length(list))
             }
           }
         
-        Y[1:changecent]<-paste(substr(cent_start,1,2),Y[1:changecent],sep="")         # allen Werten vor dem JHD-changecent die AnfangsJHD-Zahl vorne anf?gen
-        Y[-(1:changecent)]<-paste(substr(cent_end,1,2),Y[-(1:changecent)],sep="")   # allen Werten nach dem JHD-changecent die EndJHD-Zahl vorne anf?gen
+        Y[1:changecent]<-paste(substr(cent_start,1,2),Y[1:changecent],sep="")         # values before century switch
+        Y[-(1:changecent)]<-paste(substr(cent_end,1,2),Y[-(1:changecent)],sep="")   # values after century switch
         
       }
     
-    # zu speichernde Tabelle aufbauen und f?llen
+    # generate table to be saved
+    
     savetab<-matrix(nrow=dim(values)[1],ncol=7)
     savetab[,1]<-"rg1"
     savetab[,2]<-Y
@@ -69,6 +75,7 @@ for (n in 1:length(list))
     savetab[,6]<-substr(values$T,9,10) # Spalte f?r Minute
     savetab[,7]<-values$mm             # Spalte f?r valuesert in [mm]
     
-    # Tabelle im Ordner als .dat-Datei speichern
+    # write new file
+    
     write.table(savetab,paste(filepath,"/",infos[2,1],".dat",sep=""),append=F,sep=" ",dec=".",row.names=F,col.names=F,quote=F)
  }
