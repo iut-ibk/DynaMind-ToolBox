6dc684a83e8c4a27fac0af6ee2cafde279657eb8
Christian Mikovits <christian.mikovits@uibk.ac.at>
2014-12-03 14:06:07 +0100

    unified Cmakelists

--- src/scripts/eulergen.r
+++ src/scripts/eulergen.r
@@ -1,9 +1,19 @@
 ###
 ### Euler Type 2 Rainfall generator ###
 ###
-
 ### Input: r15 values in an CSV file
+###
+### Output: Euler Type 2 rainfall as a DAT (swmm5) file
 
-### read
+# cleanup
 
+rm(list=ls())
+
+file <- ("/home/gaelic/Code/DynaMind/DynaMind-DynAlp/data/intervalyear.csv")
+
+# read
+
+intervalyear <- read.csv("~/Code/DynaMind/DynaMind-DynAlp/data/intervalyear.csv")
+
+# write
 
