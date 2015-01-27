546244a07ac7072059e92d320be34ca6024af64c
chmuibk <christian.mikovits@uibk.ac.at>
2015-01-27 17:00:02 +0100

    reader notes

--- src/scripts/swmmrepreader.r
+++ src/scripts/swmmrepreader.r
@@ -1,1 +1,18 @@
 # swmm rep reader in R
+
+#steps
+
+## read
+
+- Subcatchment Runoff Summary
+- Node Depth Summary
+- Node Depth Summary
+- Node Surcharge Summary
+- Node Flooding Summary
+- Storage Volume Summary
+- Outfall Loading Summary
+- Link Flow Summary
+- (Flow Classification Summary)
+- Conduit Surcharge Summary
+- Pumping Summary
+- else: detail results
