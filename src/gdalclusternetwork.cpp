efcc030a89d05434002b59e83bdaf166e0017718
Christian Urich <christian.urich@gmail.com>
2015-02-11 12:13:52 +1100

    clustering is working smoothly

--- src/gdalclusternetwork.cpp
+++ src/gdalclusternetwork.cpp
@@ -21,6 +21,43 @@ GDALClusterNetwork::GDALClusterNetwork()
 	this->registerViewContainers(data_stream);
 }
 
+int GDALClusterNetwork::getNodeID(long & node_id, OGRPoint &  p1, std::map<std::pair<long, long>, long > & node_list, double tolerance)
+{
+	double x = p1.getX();
+	double y = p1.getY();
+	std::pair<long, long> node_hash ((long)x*tolerance, (long)y*tolerance);
+	if (node_list.count(node_hash) == 0) {
+		node_id++;
+		node_list[node_hash] = node_id;
+		return node_id;
+	} else {
+		return node_list[node_hash];
+	}
+}
+
+void GDALClusterNetwork::walk_the_edge(std::map<long, std::pair<long, long> > & edge_list, std::map<long, std::vector<long> > & start_nodes, std::map<long, int> & edge_cluster, int & marker, int start_id, bool top)
+{
+	std::vector<long> edges = start_nodes[start_id];
+	foreach(long e_id, edges) {
+		if (edge_cluster[e_id] != -1)
+			continue;
+		//Get Edge
+		if (top)
+			marker++;
+		top = false;
+		edge_cluster[e_id] = marker;
+		std::pair<long, long> edge = edge_list[e_id];
+		long s_id = edge.first;
+		long end_id = edge.second;
+		long next_id = -1;
+		if (end_id != start_id)
+			next_id = end_id;
+		else
+			next_id = s_id;
+		walk_the_edge(edge_list, start_nodes,  edge_cluster,  marker, next_id);
+	}
+}
+
 void GDALClusterNetwork::run()
 {
 	double tolerance = 100;
@@ -28,10 +65,14 @@ void GDALClusterNetwork::run()
 
 	OGRFeature * f;
 
+	//contains a coordinate pair (x,y) and an id
 	std::map<std::pair<long, long>, long > node_list;
 
+	//edge_id, cluster id
 	std::map<long, int> edge_cluster;
-
+	std::map<long, std::pair<long, long> > edge_list;
+	std::map<long, std::vector<long> > start_nodes;
+	//std::map<long,  std::vector<long> > end_nodes;
 
 	long node_id = 0;
 
@@ -47,19 +88,46 @@ void GDALClusterNetwork::run()
 		edge->getPoint(0, &p1);
 		edge->getPoint(points-1, &p2);
 
-		double x = p1.getX();
-		double y = p2.getY();
-		int current_node = -1;
-		std::pair<long, long> node_hash ((long)x*tolerance, (long)y*tolerance);
-		if (node_list.count(node_hash) == 0) {
-			node_id++;
-			node_list[node_hash] = node_id;
-			current_node = node_id;
-		} else {
-			current_node = node_list[node_hash];
-		}
+		long start_id = getNodeID(node_id, p1, node_list, tolerance);
+		long end_id = getNodeID(node_id, p2, node_list, tolerance);
+
 		edge_cluster[f->GetFID()] = -1;
+		edge_list[f->GetFID()] = std::pair<long, long>(start_id, end_id);
+
+		if (start_nodes.count(start_id) == 0)
+			start_nodes[start_id] = std::vector<long>();
+
+		if (start_nodes.count(end_id) == 0)
+			start_nodes[end_id] = std::vector<long>();
+
+		std::vector<long> & vec_start =  start_nodes[start_id];
+		vec_start.push_back(f->GetFID());
+		std::vector<long> & vec_end = start_nodes[end_id];
+		vec_end.push_back(f->GetFID());
 	}
+
+	//Start point list
+	std::vector<long> start;
+	for (std::map<long,  std::vector<long> >::const_iterator it = start_nodes.begin(); it != start_nodes.end(); ++it) {
+		//DM::Logger(DM::Error) << "start node " << it->first << "/" << it->second.size();
+		if(it->second.size() == 1) {
+			start.push_back(it->first);
+			//DM::Logger(DM::Error) << "start node " << it->first;
+		}
+	}
+	int marker = 0;
+	for(int i = 0; i < start.size(); i++) {
+		int start_id = start[i];
+		//marker++;
+		walk_the_edge(edge_list, start_nodes, edge_cluster, marker, start_id, true);
+	}
+
+	network.resetReading();
+	while (f = network.getNextFeature()) {
+		f->SetField("cluster_id", edge_cluster[f->GetFID()]);
+	}
+
+
 
 
 }
--- src/gdalclusternetwork.h
+++ src/gdalclusternetwork.h
@@ -14,6 +14,8 @@ private:
 public:
 	GDALClusterNetwork();
 	void run();
+	int getNodeID(long &node_id, OGRPoint &p1, std::map<std::pair<long, long>, long> &node_list, double tolerance);
+	void walk_the_edge(std::map<long, std::pair<long, long> > & edge_list, std::map<long, std::vector<long> > & start_nodes, std::map<long, int> & edge_cluster, int & marker, int start_id, bool top = false);
 };
 
 #endif // GDALCLUSTERNETWORK_H
