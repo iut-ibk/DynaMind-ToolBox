bda3be3e7de56267a95063db134efe840c4f04be
Christian Urich <christian.urich@gmail.com>
2015-02-12 11:01:29 +1100

    added more data

--- src/gdalclusternetwork.h
+++ src/gdalclusternetwork.h
@@ -10,11 +10,12 @@ class DM_HELPER_DLL_EXPORT GDALClusterNetwork : public DM::Module
 private:
 
 	DM::ViewContainer network;
+	DM::ViewContainer junctions;
 
 public:
 	GDALClusterNetwork();
 	void run();
-	int getNodeID(long &node_id, OGRPoint &p1, std::map<std::pair<long, long>, long> &node_list, double tolerance);
+	int getNodeID(long &node_id, OGRPoint &p1, std::map<std::pair<long, long>, long> &node_list, double tolerance, double elev = 0);
 	void walk_the_edge(std::map<long, std::pair<long, long> > & edge_list, std::map<long, std::vector<long> > & start_nodes, std::map<long, int> & edge_cluster, int & marker, int start_id, bool top = false);
 };
 
