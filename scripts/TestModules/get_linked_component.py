476d080744247ac754e6d96f36eb27eaf47b25ac
Christian Urich <christian.urich@gmail.com>
2015-03-29 20:46:43 +1100

    try without creating index

--- scripts/TestModules/get_linked_component.py
+++ scripts/TestModules/get_linked_component.py
@@ -44,10 +44,11 @@ class GetLinkedComponent(Module):
 
         def run(self):
 
-            #
-            self.__container_links.create_index("component_id")
             self.__container_links.reset_reading()
             self.__container.reset_reading()
+            #
+            #self.__container_links.create_index("component_id")
+
             counter = 0
             for cmp in self.__container:
                 search_id = cmp.GetFID()
