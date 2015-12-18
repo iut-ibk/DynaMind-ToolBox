9b46e9016f0b03350ad1cbc9d2c3c9117138ce28
Christian Urich <christian.urich@gmail.com>
2015-12-18 17:38:34 +1100

    fix problem with sftp connection

--- DynaMind-Performance-Assessment/scripts/DMPerformance/mikewhisperer.py
+++ DynaMind-Performance-Assessment/scripts/DMPerformance/mikewhisperer.py
@@ -40,50 +40,81 @@ class MikeWhisperer(Module):
             self.dummy = ViewContainer("city", COMPONENT, MODIFY)
             self.registerViewContainers([self.dummy])
 
-        def check_for_done(self, con, folder, expected):
+            self.transport = None
+            self.sftp = None
+
+        def check_for_done(self,  folder, expected):
+            self.connect()
             log("check " + str(datetime.datetime.now()) + " " + str(folder), Standard)
             try:
                 for e in expected:
                     print e
-                    con.listdir(folder + "/" + e)
+                    self.sftp.listdir(folder + "/" + e)
                 log("all done", Standard)
+                self.close()
                 return True
             except:
                 log("continue waiting", Standard)
+                self.close()
                 return False
 
-        def get_files(self, con, folder, expected, outputfolder):
-            print expected
+        def get_files(self, folder, expected, outputfolder):
+            self.connect()
             for e in expected:
                 folder_name = folder + "/" + e
                 print folder_name
-                files = con.listdir(folder_name)
+                files = self.sftp.listdir(folder_name)
                 if not os.path.exists(outputfolder + "/" + e):
                     os.makedirs(outputfolder + "/" + e)
 
                 for f in files:
                     log("copy " +  str((folder_name + "/" + f)), Standard)
-                    con.get(folder_name + "/" + f, outputfolder + "/" + e + "/" + f)
+                    self.sftp.get(folder_name + "/" + f, outputfolder + "/" + e + "/" + f)
+            self.close()
+
+        def connect(self):
+            establised = False
+            while not establised:
+                try:
+                    log(str(self.host) + " " + str(self.port) + " " + str(self.username) + " " + str(self.password), Standard)
+                    self.transport = paramiko.Transport((self.host, self.port))
+
+                    self.transport.connect(username=self.username, password=self.password)
+                    establised = True
+                except:
+                    log("Connection failed, try again", Warning)
+                    time.sleep(60)
+
+            log("connected", Standard)
+            self.sftp = paramiko.SFTPClient.from_transport(self.transport)
+
+        def close(self):
+            log("close connection", Standard)
+            self.sftp.close()
+            self.transport.close()
+
+            self.sftp = None
+            self.transport = None
 
         def run(self):
             log("try to connect", Standard)
-
+            print self.get_group_counter()
             if self.get_group_counter() != -1 and (self.get_group_counter() -  1) % self.step != 0:
                 return
 
-            log(str(self.host) + " "  + str(self.port) + " " + str(self.username) + " " + str(self.password), Standard)
-            transport = paramiko.Transport((self.host, self.port))
 
-            transport.connect(username=self.username, password=self.password)
-
-            log("connected", Standard)
-            sftp = paramiko.SFTPClient.from_transport(transport)
             result_folder = self.result_folder
             if self.get_group_counter() != -1:
                 result_folder = result_folder + "_" + str(self.get_group_counter())
-            while not self.check_for_done(sftp, result_folder, self.simulation.split(",")):
+            while not self.check_for_done(result_folder, self.simulation.split(",")):
                 time.sleep(60)
 
-            self.get_files(sftp, result_folder, self.simulation.split(","), self.output_folder)
-            sftp.close()
+            self.get_files(result_folder, self.simulation.split(","), self.output_folder)
+
+
+
+
+
+
+
 
