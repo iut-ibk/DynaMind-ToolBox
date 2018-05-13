f10e6b6574d3c548a048d53fdbf8837207a90b4e
Christian Urich <christian.urich@gmail.com>
2018-05-13 15:19:03 +1000

    added in memory processing

--- DynaMind-GDALModules/scripts/GDALModules/dm_value_from_raster.py
+++ DynaMind-GDALModules/scripts/GDALModules/dm_value_from_raster.py
@@ -29,9 +29,10 @@ class DM_ValueFromRaster(Module):
         def init(self):
             self.node_view = ViewContainer(self.view_name, NODE, READ)
             self.node_view.addAttribute(self.attribute_name, Attribute.DOUBLE, WRITE)
+            self.catchment_view = ViewContainer("city", FACE, READ)
 
 
-            self.registerViewContainers([self.node_view])
+            self.registerViewContainers([self.node_view, self.catchment_view])
 
         def run(self):
             dataset = gdal.Open( self.raster_file, GA_ReadOnly)
@@ -52,27 +53,62 @@ class DM_ValueFromRaster(Module):
 
             #(353136,5776456)
             ct = osr.CoordinateTransformation(srsLatLong, srs)
+            inMemory = True
+            if inMemory:
+                for c in self.catchment_view:
+                    geom = c.GetGeometryRef()
+                    env = geom.GetEnvelope()
+                    p1 = ct.TransformPoint(env[0], env[2])
+                    p2 = ct.TransformPoint(env[1], env[3])
+                    minx = int((p1[0]-gt[0])/gt[1])
+                    miny = int((p1[1]-gt[3])/gt[5])
+                    maxx = int((p2[0]-gt[0])/gt[1])
+                    maxy = int((p2[1]-gt[3])/gt[5])
+
+                    if miny > maxy:
+                        min_y_tmp = miny
+                        miny = maxy
+                        maxy = min_y_tmp
+                        log("swapsy", Standard)
+                    minx -= 5
+                    miny -= 5
+                    maxx += 5
+                    maxy += 5
+                    print str(minx) + "/" + str(miny)+ "/" + str(maxx)+ "/" + str(maxy), maxx-minx, maxy-miny
+                    log(str(minx) + "/" + str(miny)+ "/" + str(miny)+ "/" + str(maxy), Standard)
+
+                    values = band.ReadAsArray(minx, miny, maxx-minx, maxy-miny)
+
+
 
             for node in self.node_view:
                 geom = node.GetGeometryRef()
                 point = ct.TransformPoint(geom.GetX(), geom.GetY())
                 x = (point[0]-gt[0])/gt[1]
                 y = (point[1]-gt[3])/gt[5]
-                datatype = band.DataType
 
-                scanline = band.ReadRaster( int(x), int(y), 1, 1, 1, 1, datatype)
-                if not scanline:
-                    log("No value found for " + str(x) + "," + str(y), Warning)
-                    continue
+                if inMemory:
+                    val = values[int(y)-miny][int(x)-minx]
 
-                if datatype == GDT_Int32:
-                    tuple_of_floats = struct.unpack('i' * 1, scanline)
-                elif datatype == GDT_Float32:
-                    tuple_of_floats = struct.unpack('f' * 1, scanline)
-                else:
-                    log("Datatype " + str(datatype) + " not supported", Error)
-                    self.node_view.finalise()
-                    return
-                node.SetField(self.attribute_name, tuple_of_floats[0])
+                #print val.item()
+                    node.SetField(self.attribute_name, val.item())
+                else
+                    datatype = band.DataType
+
+                    scanline = band.ReadRaster( int(x), int(y), 1, 1, 1, 1, datatype)
+                    if not scanline:
+                        log("No value found for " + str(x) + "," + str(y), Warning)
+                        continue
+
+                    if datatype == GDT_Int32:
+                        tuple_of_floats = struct.unpack('i' * 1, scanline)
+                    elif datatype == GDT_Float32:
+                        tuple_of_floats = struct.unpack('f' * 1, scanline)
+                    else:
+                        log("Datatype " + str(datatype) + " not supported", Error)
+                        self.node_view.finalise()
+                        return
+
 
             self.node_view.finalise()
+            self.catchment_view.finalise()
